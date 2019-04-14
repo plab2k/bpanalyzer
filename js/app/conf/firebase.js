1dd040b9889becddf129142d50dd6818c2dcd24a
plab2k <plab2k@gmail.com>
2019-04-14 16:23:40 +0300

    test

---
+++ js/app/conf/firebase.js
@@ -0,0 +1,10 @@
+;
+var app = app || { config: {} };
+app.config.firebase = {
+    "apiKey": "AIzaSyCbb8jTiag_He6CYa1w1iflMLrOR2yYHlk",
+    "authDomain": "bpanalyzer-db.firebaseapp.com",
+    "databaseURL": "https://bpanalyzer-db.firebaseio.com",
+    "projectId": "bpanalyzer-db",
+    "storageBucket": "bpanalyzer-db.appspot.com",
+    "messagingSenderId": "669550904949"
+};
--- js/modules/firebase.js
+++ js/modules/firebase.js
@@ -2,19 +2,11 @@
 var app = app || {};
 app.firebase = (function() {
     "use strict";
-    const config = {
-        apiKey: "AIzaSyCbb8jTiag_He6CYa1w1iflMLrOR2yYHlk",
-        authDomain: "bpanalyzer-db.firebaseapp.com",
-        databaseURL: "https://bpanalyzer-db.firebaseio.com",
-        projectId: "bpanalyzer-db",
-        storageBucket: "bpanalyzer-db.appspot.com",
-        messagingSenderId: "669550904949"
-    };
 
     return {
         init: function(callback) {
             app.helper.getScript("https://www.gstatic.com/firebasejs/live/3.0/firebase.js", () => {
-                firebase.initializeApp(config);
+                firebase.initializeApp(app.config.firebase);
 
                 app.serviceWorker.pushManager.getSubscription().then(function(sub) {
                     if (sub === null) {
