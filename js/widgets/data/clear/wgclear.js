e27e128ab43d58e17039841a01d8f90072a6f3fe
Dmitry Lisutin <plab2k@gmail.com>
2019-04-30 16:48:28 +0300

    add Roboto

--- js/widgets/data/clear/wgclear.js
+++ js/widgets/data/clear/wgclear.js
@@ -3,8 +3,14 @@ $.widget("ba.wgclear", $.ba.wgbase, {
 
   _create: function () {
     const that = this;
+    $("[data-text=Confirm]", this.element).hide();
     this._on(this.element, {
       "click a": (event) => {
+        if ($(event.target).attr("data-text") != "Confirm") {
+          $(event.target).hide();
+          $("[data-text=Confirm]", this.element).show();
+          return;
+        }
         app.control.waiting.start();
         const objnames = app.db.getTableNames();
         Promise.all(
@@ -13,6 +19,8 @@ $.widget("ba.wgclear", $.ba.wgbase, {
           }))
           .then(() => {
             app.control.waiting.end();
+            $("[data-text=Clear]", that.element).show();
+            $("[data-text=Confirm]", that.element).hide();
             that._trigger("onSuccess");
           });
       }
