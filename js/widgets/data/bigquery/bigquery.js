c8e805d1cf82e8c44333d9fbaaf598297b2cc29b
Dmitry Lisutin <plab2k@gmail.com>
2019-05-21 15:11:21 +0300

    add download demo

---
+++ js/widgets/data/bigquery/bigquery.js
@@ -0,0 +1,42 @@
+/**
+<script src="https://apis.google.com/js/api.js"></script>
+<script>
+
+ * Sample JavaScript code for bigquery.tabledata.insertAll
+ * See instructions for running APIs Explorer code samples locally:
+ * https://developers.google.com/explorer-help/guides/code_samples#javascript
+ */
+
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({ scope: "https://www.googleapis.com/auth/bigquery https://www.googleapis.com/auth/bigquery.insertdata https://www.googleapis.com/auth/cloud-platform" })
+        .then(function () { console.log("Sign-in successful"); },
+            function (err) { console.error("Error signing in", err); });
+}
+function loadClient() {
+    gapi.client.setApiKey("YOUR_API_KEY");
+    return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/bigquery/v2/rest")
+        .then(function () { console.log("GAPI client loaded for API"); },
+            function (err) { console.error("Error loading GAPI client for API", err); });
+}
+// Make sure the client is loaded and sign-in is complete before calling this method.
+function execute() {
+    return gapi.client.bigquery.tabledata.insertAll({
+        "projectId": "vcvcvc",
+        "datasetId": "vvcvc",
+        "tableId": "vv",
+        "resource": {}
+    })
+        .then(function (response) {
+            // Handle the results here (response.result has the parsed body).
+            console.log("Response", response);
+        },
+            function (err) { console.error("Execute error", err); });
+}
+gapi.load("client:auth2", function () {
+    gapi.auth2.init({ client_id: "YOUR_CLIENT_ID" });
+});
+
+/* <button onclick="authenticate().then(loadClient)">authorize and load</button>
+<button onclick="execute()">execute</button>
+ */
---
+++ js/widgets/data/bigquery/wgbigquery.js
@@ -0,0 +1,31 @@
+"use strict";
+$.widget("ba.bigquery", $.ba.wgbase, {
+
+    _create: function () {
+        const that = this;
+        $("[data-text=Confirm]", this.element).hide();
+        this._on(this.element, {
+            "click a": (event) => {
+                if ($(event.target).attr("data-text") != "Confirm") {
+                    $(event.target).hide();
+                    $("[data-text=Confirm]", this.element).show();
+                    return;
+                }
+                app.control.waiting.start();
+                const objnames = app.db.getTableNames();
+                Promise.all(
+                    objnames.map(name => {
+                        app.db.clear(name)
+                    }))
+                    .then(() => {
+                        app.control.waiting.end();
+                        $("[data-text=Clear]", that.element).show();
+                        $("[data-text=Confirm]", that.element).hide();
+                        that._trigger("onSuccess");
+                    });
+            }
+        });
+        this._super();
+    }
+
+});
